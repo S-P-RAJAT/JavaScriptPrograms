7bbc0668f341f56996c404d191ccda9a92c59a88
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-10-05 20:46:30 +0530

    ADDED: Find sum & average of five two digit random numbers

--- PractisePrograms/sequence_practise_problem.js
+++ PractisePrograms/sequence_practise_problem.js
@@ -7,3 +7,10 @@ console.log("Dice Roll = "+diceValue);
 let diceValue1 = 1 + Math.floor(Math.random() * 10)%6;
 let diceValue2 = 1 + Math.floor(Math.random() * 10)%6;
 console.log("sum = "+(diceValue1+diceValue2));
+
+sum = 0
+for(let i=0; i<5;i++)
+    sum += Math.floor(Math.random() * 100);
+
+let avg = sum/5;
+console.log("sum = "+sum+" average = "+avg);
