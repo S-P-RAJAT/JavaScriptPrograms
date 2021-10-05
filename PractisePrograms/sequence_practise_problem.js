51790a9cf6b065735003f7573a4c99f18554a5c3
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-10-05 20:39:18 +0530

    ADDED: Find sum of two dice numbers

--- PractisePrograms/sequence_practise_problem.js
+++ PractisePrograms/sequence_practise_problem.js
@@ -3,3 +3,7 @@ console.log("Digit = "+digit);
 
 let diceValue = 1 + Math.floor(Math.random() * 10)%6;
 console.log("Dice Roll = "+diceValue);
+
+let diceValue1 = 1 + Math.floor(Math.random() * 10)%6;
+let diceValue2 = 1 + Math.floor(Math.random() * 10)%6;
+console.log("sum = "+(diceValue1+diceValue2));
