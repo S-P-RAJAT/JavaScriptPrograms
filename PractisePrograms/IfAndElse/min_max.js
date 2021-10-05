772b3b554843d94ab61358aa9538aa5ace6692aa
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-10-05 21:47:38 +0530

    REFACTORED: Used if statement to find minimum and maximum

--- PractisePrograms/IfAndElse/min_max.js
+++ PractisePrograms/IfAndElse/min_max.js
@@ -3,8 +3,10 @@ maximum = Number.MIN_VALUE
 
 for (let index = 0; index < 5; index++) {
     let digit = Math.floor(Math.random() * 1000);
-    maximum = (digit > maximum) ? digit : maximum;
-    minimum = (digit < minimum) ? digit : minimum;
+    if (digit > maximum)
+        maximum = digit
+    if (digit < minimum)
+        minimum = digit
 }
 
-console.log("Maximum = "+maximum,"\nMinimum = "+minimum)
+console.log("Maximum = " + maximum, "\nMinimum = " + minimum)
