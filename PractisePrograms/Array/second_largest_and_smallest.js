b7be4f1020adb8b93a77288a27279ff6e8ebfc6a
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-10-07 01:14:26 +0530

    ADDED: Program to find second smallest & largest value of sorted array

---
+++ PractisePrograms/Array/second_largest_and_smallest.js
@@ -0,0 +1,13 @@
+let array = new Array();
+let arraySize = 10;
+for (let index = 0; index < arraySize; index++) {
+    let digit = Math.floor(Math.random() * 1000);
+    array.push(digit);
+}
+console.log("Array before sorting: ", array);
+
+array.sort();
+
+console.log("Array after sorting: ", array);
+console.log("Second largest = ",array[1]);
+console.log("Second smallest = ",array[arraySize-2]);
